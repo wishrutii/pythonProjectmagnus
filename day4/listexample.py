05658ca951603f100f659ecfb3ac7467a5313016
Vishruti Patel <vishruti0117@gmail.com>
2023-07-28 23:34:43 -0400

    Day4_26_July_2023

---
+++ day4/listexample.py
@@ -0,0 +1,23 @@
+a1=[10,30,23,'abc',10.5,'hello']
+
+print(a1[3])
+print(a1[3:6])
+print(a1[2:])
+print(a1[:6])
+print(a1)
+a1.append(120) # Appending(adding) the data to the existing list
+print(a1)
+a1.insert(3,'hi') # Inserting the value at a specific index point/position
+print(a1)
+a1[6]="vaah beta" # Replacing the Value at a specific index point/position
+print(a1)
+a1.remove('abc') # Removing a specific value from the list
+print(a1)
+a1.pop() # used to remove the last value form the list
+print(a1)
+a1.pop(4) # removes the specific index position you want to remove.
+print(a1)
+# Overall notes key points: remove function removes the actual value from the [] list, and pop function removes
+# the value by using the specific index position.
+
+
